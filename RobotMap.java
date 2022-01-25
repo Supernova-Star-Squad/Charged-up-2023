b1050f08faf50218b8eb547f89618397ce0f0908
HOLMESBY <98364879+HOLMESBY@users.noreply.github.com>
2022-01-25 13:15:51 +1100

    Update RobotMap.java

--- RobotMap.java
+++ RobotMap.java
@@ -16,7 +16,6 @@ public class RobotMap {
 
     // Controllers //
     public static int joystickPort = 0;
-    public static int xBoxPort = 1;
     public Joystick logitech = new Joystick(RobotMap.joystickPort);
 
     // Motors //
@@ -25,32 +24,7 @@ public class RobotMap {
     public static int leftRearMotor = 1;
     public static int rightFrontMotor = 2;
     public static int rightRearMotor = 3;
-        // Actuator Motors //
-    public static int intakeMotor = 12;
-    public static int spinnerMotor = 13;
-    public static int forkliftMotor = 14;
-    public static int shooterMotor = 15;
-
-
-    // Controller Buttons //
-    /* CANT GET BUTTONS TO WORK, USING RAW INPUT INSTEAD
-    public static int shootButton = 1;
-    public static int shootButtonReverse = 7;
-    public static int elevatorUpButton = 2;
-    public static int elevatorDownButton = 3;
-    public static int forkliftUpButton = 4;
-    public static int forkliftDownButton = 11;
-    public static int intakeLoadButton = 12;
-    public static int intakeUnloadButton = 9;
-    */
-
+       
     // Throttle Setting //
     public static double maxThrottle = 0.7;
-    public static double shooterThrottle =0.4;
-    public static double forkliftThrottle = 0.4;
-    public static double spinnerThrottle = 0.2;
-    public static double intakeThrottle = 0.4;
-
-
-
-}
+    {
